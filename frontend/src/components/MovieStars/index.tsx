be783a3b15b8f29e7a02e641edb575f9e1ac5cf2
Eduardo Cabral <eduardocabral941@gmail.com>
2022-03-12 21:07:52 -0300

    Save, useNavigate

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -6,6 +6,7 @@ import './styles.css'
 type Props = {
     score: number;
 }
+
 type StarProps = {
     fill: number;
 }
@@ -31,10 +32,10 @@ function getFills(score: number) {
 }
 
 function Star({ fill }: StarProps){
-    if (fill == 0){
+    if (fill === 0){
         return <StarEmpty />
     }
-    else if (fill == 1){
+    else if (fill === 1){
         return <StarFull/>
     }
     else {
